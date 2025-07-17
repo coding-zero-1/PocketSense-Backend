fa737bc581492ef6af6e9ee992b3e6331d4d39c0
coding-zero-I <yashsangwan4700@gmail.com>
2025-07-17 18:26:04 +0530

    added auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,26 @@
+import { Request,Response,NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const token = req.headers.token;
+        if (!token) {
+            res.status(401).json({ 
+                success: false,
+                error: "Unauthorized" 
+            });
+            return;
+        }
+        // Verify the JWT token
+        const decoded = jwt.decode(token as string) as { clerkId: string };
+        req.clerkId = decoded?.clerkId;
+        next();
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"Internal server error please wait!"
+        })
+    }
+}
+
+export default authMiddleware;
